6a467b4f9547bed3b99d03d414f8fb4c4e4da111
Wynand <groendrakie@gmail.com>
2017-04-06 16:19:17 +0200

    fix modal materialize scss dependancy

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mozfet:autoform-materialize',
   summary: 'Materialize theme for Autoform',
-  version: '0.1.8',
+  version: '0.1.9',
   git: 'https://github.com/mozfet/meteor-autoform-materialize.git'
 });
 
